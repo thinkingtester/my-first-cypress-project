32abc36c3b59837ce2b25ad37da7bd21c456c34a
Kristin Jackvony <kristinjackvony@Kristins-MacBook-Pro.local>
2024-03-28 21:05:42 -0400

    Update Contact and negative tests

--- cypress/e2e/addContact.spec.cy.js
+++ cypress/e2e/addContact.spec.cy.js
@@ -1,24 +1,34 @@
 describe('Add Contact Tests', () => {
     it('Can add a new contact', () => {
 
-      let random = Math.floor(Math.random() * 1000)
+        let random = Math.floor(Math.random() * 1000)
 
-      cy.login()    
-      cy.addContact(random)
+        cy.login()    
+        cy.addContact(random)
 
-      cy.contains('Test' + random + ' User').click()
-      cy.get('#firstName').should('contain', `Test${random}`)
-      cy.get('#lastName').should('contain', 'User')
-      cy.get('#birthdate').should('contain', '2000-01-02')
-      cy.get('#email').should('contain', 'foo@bar.com')
-      cy.get('#phone').should('contain', '8005001000')
-      cy.get('#street1').should('contain', '123 Main St.')
-      cy.get('#street2').should('contain', 'Apt. 1A')
-      cy.get('#city').should('contain', 'Boston')
-      cy.get('#stateProvince').should('contain', 'MA')
-      cy.get('#postalCode').should('contain', '12345')
-      cy.get('#country').should('contain', 'USA')
+        cy.contains('Test' + random + ' User').click()
+        cy.get('#firstName').should('contain', `Test${random}`)
+        cy.get('#lastName').should('contain', 'User')
+        cy.get('#birthdate').should('contain', '2000-01-02')
+        cy.get('#email').should('contain', 'foo@bar.com')
+        cy.get('#phone').should('contain', '8005001000')
+        cy.get('#street1').should('contain', '123 Main St.')
+        cy.get('#street2').should('contain', 'Apt. 1A')
+        cy.get('#city').should('contain', 'Boston')
+        cy.get('#stateProvince').should('contain', 'MA')
+        cy.get('#postalCode').should('contain', '12345')
+        cy.get('#country').should('contain', 'USA')
 
-      cy.deleteContact()
+        cy.deleteContact()
     })
-  })
+    
+    it('Returns an error when required field is missing', () => {
+
+        cy.login() 
+        cy.get('#add-contact').click()
+        cy.get('#firstName').type('Prunella')
+        cy.get('#submit').click()
+        cy.get('#error').should('contain', 'Contact validation failed: lastName')
+    })
+
+})
---
+++ cypress/e2e/updateContact.spec.cy.js
@@ -0,0 +1,55 @@
+describe('Update Contact Tests', () => {
+    it('Can update a contact', () => {
+
+      let random = Math.floor(Math.random() * 1000)
+
+      cy.login()    
+      cy.addContact(random)
+
+      cy.contains('Test' + random + ' User').click()
+      cy.get('#edit-contact').click()
+      cy.get('#firstName').clear().type('Update' + random)
+      cy.get('#lastName').clear().type('Person')
+      cy.get('#birthdate').clear().type('2001-02-03')
+      cy.get('#email').clear().type('bar@foo.com')
+      cy.get('#phone').clear().type('8005551001')
+      cy.get('#street1').clear().type('234 Cypress Ave.')
+      cy.get('#street2').clear().type('Apt. 2B')
+      cy.get('#city').clear().type('New York')
+      cy.get('#stateProvince').clear().type('NY')
+      cy.get('#postalCode').clear().type('23456')
+      cy.get('#country').clear().type('United States')
+      cy.get('#submit').click()
+      cy.get('#return').click()
+
+      cy.contains('Update' + random + ' Person').click()
+      cy.get('#firstName').should('contain', `Update${random}`)
+      cy.get('#lastName').should('contain', 'Person')
+      cy.get('#birthdate').should('contain', '2001-02-03')
+      cy.get('#email').should('contain', 'bar@foo.com')
+      cy.get('#phone').should('contain', '8005551001')
+      cy.get('#street1').should('contain', '234 Cypress Ave.')
+      cy.get('#street2').should('contain', 'Apt. 2B')
+      cy.get('#city').should('contain', 'New York')
+      cy.get('#stateProvince').should('contain', 'NY')
+      cy.get('#postalCode').should('contain', '23456')
+      cy.get('#country').should('contain', 'United States')
+
+      cy.deleteContact()
+    })
+
+    it('Returns error when birthdate validation fails', () => {
+
+      let random = Math.floor(Math.random() * 1000)
+
+      cy.login()  
+      cy.addContact(random)
+      cy.contains('Test' + random + ' User').click()
+      cy.get('#edit-contact').click()
+      cy.get('#birthdate').clear().type('02-03-2001')
+      cy.get('#submit').click()
+      cy.get('#error').should('contain', 'Birthdate is invalid')
+      cy.get('#cancel').click()
+      cy.deleteContact()
+    })
+  })
